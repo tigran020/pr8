001ba7e6d1a8ee6761e53b6a35ea123d4f90b52d
asernew <tigrank.19@mail.ru>
2020-10-21 18:20:52 +0300

    Initial commit

---
+++ src/com/company/UnfairWaitList.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class UnfairWaitList<E> extends WaitList<E> {
+
+    public UnfairWaitList() {
+    }
+
+    public void remove(E element) {
+        content.remove(element);
+    }
+
+    public void moveToBack(E element) {
+        content.add(content.remove());
+    }
+
+    @Override
+    public String toString() {
+        return "UnfairWaitList{" +
+                "content=" + content +
+                '}';
+    }
+}
